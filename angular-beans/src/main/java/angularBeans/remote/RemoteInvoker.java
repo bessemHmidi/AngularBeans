a0c59244824758dd8f071090bc318a551abfcf3a
bessem <bessem@esprit.com>
2015-01-20 16:42:58 +0100

    Signed-off-by: bessem <bessem@esprit.com>

--- angular-beans/src/main/java/angularBeans/remote/RemoteInvoker.java
+++ angular-beans/src/main/java/angularBeans/remote/RemoteInvoker.java
@@ -1,3 +1,24 @@
+/*
+ * AngularBeans, CDI-AngularJS bridge 
+ *
+ * Copyright (c) 2014, Bessem Hmidi. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ */
+
+/**
+ @author Bessem Hmidi
+ */
 package angularBeans.remote;
 
 import java.io.IOException;
