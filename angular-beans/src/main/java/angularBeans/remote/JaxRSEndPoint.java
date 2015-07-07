151ad6f29f8971cc6d92ea1495e8a73d4fca3955
bessem <bessem@esprit.com>
2015-07-07 12:24:15 +0100

    supress the JaxRSEndPoint (useless)

--- angular-beans/src/main/java/angularBeans/remote/JaxRSEndPoint.java
+++
@@ -1,9 +0,0 @@
-package angularBeans.remote;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class JaxRSEndPoint extends Application{
-
-}
