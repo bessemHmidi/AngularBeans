3de286653629299f53a15878ce59efbe227d3e77
Aymen Naili <naili@live.com>
2016-06-18 17:23:32 -0600

    Changed Accessors fron Enum type to class (static class).

---
+++ angular-beans/src/main/java/angularBeans/util/Accessors.java
@@ -0,0 +1,26 @@
+package angularBeans.util;
+
+/**
+ * Helper class for setters, getters perfixes.
+ * 
+ * @author melbehi
+ * @author Aymen Naili
+ */
+public final class Accessors {
+
+	/**
+	 * Getter method prefix.
+	 */
+	public static String GETTER_PREFIX = "get";
+
+	/**
+	 * Setter method prefix.
+	 */
+	public static String SETTER_PREFIX = "set";
+
+	/**
+	 * Boolean getter method preix.
+	 */
+	public static String BOOLEAN_GETTER_PREFIX = "is";
+	
+}
