576fbfa28dfe9b58f7d2bf240e0001c4399adb58
Osni Marin <osnimarin@gmail.com>
2016-09-30 16:12:33 -0300

    Update Constants.java

--- angular-beans/src/main/java/angularBeans/util/Constants.java
+++ angular-beans/src/main/java/angularBeans/util/Constants.java
@@ -1,3 +1,4 @@
+
 package angularBeans.util;
 
 /**
@@ -9,16 +10,23 @@ package angularBeans.util;
  *
  */
 public final class Constants {
-	/* Both 0 and 1 is primitive values and do not need to be declared in this class. Otherwise, please add your
-	 * immutable values below. */
+   /*
+    * Both 0 and 1 is primitive values and do not need to be declared in this class. Otherwise,
+    * please add your immutable values below.
+    */
 
-	/**
-	 * Int value of 2.
-	 */
-	public static final int TWO = 2;
+   /**
+    * Int value of 2.
+    */
+   public static final int TWO = 2;
 
-	/**
-	 * Int value of 3.
-	 */
-	public static final int THREE = 3;
+   /**
+    * Int value of 3.
+    */
+   public static final int THREE = 3;
+
+   /**
+    * Value default to urlPatterns
+    */
+   public static final String URL_PATTERNS = "/http/invoke/*";
 }
