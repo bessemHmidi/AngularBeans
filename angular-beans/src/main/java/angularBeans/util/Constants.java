35a23550cccf2502f2a4d842f9d4397622026b73
Felipe Weiber <weiberfelipe@gmail.com>
2016-11-08 16:25:18 -0200

    New constants

--- angular-beans/src/main/java/angularBeans/util/Constants.java
+++ angular-beans/src/main/java/angularBeans/util/Constants.java
@@ -9,21 +9,38 @@ package angularBeans.util;
  *
  */
 public final class Constants {
-	/* Both 0 and 1 is primitive values and do not need to be declared in this class. Otherwise, please add your
-	 * immutable values below. */
+   /*
+    * Both 0 and 1 is primitive values and do not need to be declared in this class. Otherwise,
+    * please add your immutable values below.
+    */
 
-	/**
-	 * Int value of 2.
-	 */
-	public static final int TWO = 2;
+   /**
+    * Int value of 2.
+    */
+   public static final int TWO = 2;
 
-	/**
-	 * Int value of 3.
-	 */
-	public static final int THREE = 3;
-	
-	/**
-	 * Value default to urlPatterns
-	 */
-	public static final String URL_PATTERNS = "/http/invoke/*";
+   /**
+    * Int value of 3.
+    */
+   public static final int THREE = 3;
+
+   /**
+    * Value default to urlPatterns http invoke
+    */
+   public static final String URL_PATTERNS_INVOKE = "/http/invoke/*";
+   
+   /**
+    * Value default to urlPatterns real time service
+    */
+   public static final String URL_PATTERNS_SERVICE = "/rt-service/*";
+
+   /**
+    * Value LOB to LOB_BIND
+    */
+   public static final String LOB_BIND = "LOB";
+
+   /**
+    * Value BASE64 to BASE64_BIND
+    */
+   public static final String BASE64_BIND = "BASE64";
 }
